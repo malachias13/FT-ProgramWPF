432619e127418606fbb1a800e99d8a50afabc861
Malachias Harris <69220988+malachias13@users.noreply.github.com>
2025-12-18 01:43:57 -0500

    Added RPC client code and refs to project.

---
+++ FT ProgramWPF/Managers/FileCollection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FT_ProgramWPF.Managers
+{
+	public class FileCollection
+	{
+		public List<FileInfo> files = new List<FileInfo>();
+
+		public static byte[] ReadDataFromFile(string path)
+		{
+			return File.ReadAllBytes(path);
+		}
+
+		public static void WriteDataToFile(string path, byte[] data)
+		{
+			FileStream OutputFile = new FileStream(path, FileMode.Create);
+			OutputFile.Write(data);
+			OutputFile.Close();
+		}
+
+		public void OpenFolderAndGetFiles(string folder)
+		{
+			foreach (string file in Directory.EnumerateFiles(folder))
+			{
+				FileInfo fileInfo = new FileInfo(file);
+				files.Add(fileInfo);
+			}
+		}
+
+		public FileInfo? GetFile(string filename)
+		{
+			foreach (FileInfo fileInfo in files)
+			{
+				if (fileInfo.Name.Equals(filename))
+				{
+					return fileInfo;
+				}
+			}
+			return null;
+		}
+
+		public static byte[] CreateX509Certificate2Bytes(string certName, string? password)
+		{
+			var ecdsa = ECDsa.Create();
+			var rsa = RSA.Create();
+
+			var req = new CertificateRequest($"cn={certName}", rsa,
+				HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+			var cert = req.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(1));
+
+			return cert.Export(X509ContentType.Pfx, password);
+		}
+
+		public static X509Certificate2 CreateX509Certificate2(string certName)
+		{
+			var ecdsa = ECDsa.Create();
+			var rsa = RSA.Create();
+
+			var req = new CertificateRequest($"cn={certName}", rsa,
+				HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+			var cert = req.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(1));
+
+			return cert;
+		}
+	}
+}
---
+++ FT ProgramWPF/Managers/RpcClient.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FT_ProgramWPF.Managers;
+using System.Net.Http;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using RpcShared;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FT_ProgramWPF.Managers
+{
+	public class RpcClient
+	{
+
+		GrpcChannel channel;
+		TPL.TPLClient TPLClient;
+
+		public RpcClient(string serverAddress)
+		{
+
+			var cert = FileCollection.CreateX509Certificate2("client-Cert");
+
+			var baseUri = new Uri(serverAddress);
+
+			var httpClientHandler = new HttpClientHandler();
+			httpClientHandler.ClientCertificates.Add(cert);
+			httpClientHandler.ServerCertificateCustomValidationCallback =
+				HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+			channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions
+			{
+				HttpHandler = new GrpcWebHandler(httpClientHandler)
+			});
+
+			TPLClient = new TPL.TPLClient(channel);
+
+			TPLClient.SayHelloAsync(new HelloRequest { Name = "Android client!" });
+		}
+
+		// TODO Make functions to get files from the server.
+
+
+		// TODO Get All Files info {Name, Date, maybe thumbnil}
+
+		public async Task<RpcShared.FileMetadataReply> GetAllFilesMetadataAsync()
+		{
+			var input = new GetAllFileMetadataRequest();
+			var reply = await TPLClient.GetAllFilesMeatadataAsync(input);
+
+			return reply;
+		}
+
+		public FileMetadataReply GetAllFilesMetadata()
+		{
+			var input = new GetAllFileMetadataRequest();
+			var reply = TPLClient.GetAllFilesMeatadata(input);
+
+			return reply;
+		}
+
+
+
+		// ToDO Get File by name {download}
+		public AsyncServerStreamingCall<FileReplay> GetFile(string filename)
+		{
+			var input = new GetFileRequest { Filename = filename };
+			var reply = TPLClient.GetFile(input);
+
+			return reply;
+		}
+
+		// Could send file to server but I may just make it readonly.
+	}
+}
