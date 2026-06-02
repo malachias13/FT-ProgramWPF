1b6eeb65621b4b0dfdb6d69fb482cd90e4236978
Malachias Harris <69220988+malachias13@users.noreply.github.com>
2026-06-02 13:17:29 -0400

    Added scroll view for text logs

---
+++ FT ProgramWPF/Model/ScrollViewerBehavior.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace FT_ProgramWPF.Model
+{
+	public static class AutoScrollBehavior
+	{
+		public static readonly DependencyProperty AutoScrollProperty =
+			DependencyProperty.RegisterAttached("AutoScroll", typeof(bool), typeof(AutoScrollBehavior), new PropertyMetadata(false, OnAutoScrollChanged));
+
+		public static void SetAutoScroll(DependencyObject obj, bool value) => obj.SetValue(AutoScrollProperty, value);
+		public static bool GetAutoScroll(DependencyObject obj) => (bool)obj.GetValue(AutoScrollProperty);
+
+		private static void OnAutoScrollChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			if (d is ItemsControl itemsControl && (bool)e.NewValue)
+			{
+				itemsControl.Loaded += (s, args) =>
+				{
+					// Ensure we are listening to the correct collection
+					if (itemsControl.ItemsSource is INotifyCollectionChanged collection)
+					{
+						collection.CollectionChanged += (sender, eventArgs) =>
+						{
+							if (eventArgs.Action == NotifyCollectionChangedAction.Add && itemsControl.Items.Count > 0)
+							{
+								// Critical: Use Dispatcher to wait for UI rendering
+								itemsControl.Dispatcher.BeginInvoke(new Action(() =>
+								{
+									var lastItem = itemsControl.Items[^1];
+									//itemsControl.ScrollIntoView(lastItem);
+									itemsControl.BringIntoView();
+								}), System.Windows.Threading.DispatcherPriority.Loaded);
+							}
+						};
+					}
+				};
+			}
+		}
+	}
+}
--- FT ProgramWPF/ViewModel/HostingViewModel.cs
+++ FT ProgramWPF/ViewModel/HostingViewModel.cs
@@ -1,17 +1,56 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace FT_ProgramWPF.ViewModel
 {
 	public partial class HostingViewModel : ObservableObject
 	{
+		public ObservableCollection<string> LogEntries { get; } = new ObservableCollection<string>();
+
+		public ScrollViewer LogScrollViewer { get; set; }
+
 		public HostingViewModel()
 		{
 
+			//LogEntries.CollectionChanged += LogEntries_CollectionChanged;
+			Application.Current.Dispatcher.Invoke(async () =>
+			{
+
+				for (int i = 0; i < 100; i++)
+				{
+					AddLogEntry($"{i}: Hello ffkmr rfkrkmfkmr krkf kr kf kr kf kr " +
+						$"rkflmrmfmmeomofm4ompofmmfomopm emfopmewpofmopmw fowmpofm wmfwmpo" +
+						$"wofmwkrefm fowmpfmwpf wmfpwm fjwmporm wmfpwmmrw" +
+						$"wormpwmef wekrpkmrfmwplefmwepokrr. END!");
+
+					await Task.Delay(300);
+				}
+			});
 		}
+
+		public void AddLogEntry(string text)
+		{
+			LogEntries.Add(text);
+		}
+
+		//private void LogEntries_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+		//{
+		//	if (e.Action == NotifyCollectionChangedAction.Add)
+		//	{
+		//		// Use Dispatcher to ensure UI is updated before scrolling
+		//		Application.Current.Dispatcher.InvokeAsync(() =>
+		//		{
+		//			LogScrollViewer.ScrollToEnd();
+		//		}, System.Windows.Threading.DispatcherPriority.Loaded);
+		//	}
+		//}
 	}
 }
